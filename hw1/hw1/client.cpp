a0c7443fd9308bb3c443832966af4b59cdbf5393
Patrick Sheehan <patrickanthony.sheehan@yahoo.com>
2015-01-27 00:20:59 -0600

    Add file for TCP client

---
+++ hw1/hw1/client.cpp
@@ -0,0 +1,23 @@
+/*
+* Patrick Sheehan
+* CSCE463 HW1
+* 26 January 2015
+*
+*/
+
+#include "stdafx.h"
+
+void socketTest()
+{
+	// Open a socket
+
+	// Get IP address of server in URL
+
+	// Initiate connection on correct port
+
+	// Send request conforming to correct protocol
+
+	// Receive response
+
+	// Close socket
+}
