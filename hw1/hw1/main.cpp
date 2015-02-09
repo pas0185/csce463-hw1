d1cbb4cfa0d09584f3b1155e729bb849a1101bfe
Patrick Sheehan <patrickanthony.sheehan@yahoo.com>
2015-02-09 14:59:00 -0600

    Clean up main.cpp, annotate for p3

--- hw1/hw1/main.cpp
+++ hw1/hw1/main.cpp
@@ -10,60 +10,11 @@ void htmlParserTest();
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	if (argc < 2)
+	if (argc == 3)
 	{
-		printf("No input file found in arguments. Please try again using the sytax:\n\thw1.exe <URL>\n");
-		return 0;
-	}
-	else if (argc == 2)
-	{
-		// > hw1.exe <URL>
-
-		// part 1 functionality
-		int statusCode;
-		char *url, *request, *hostname, *htmlFileName;
-		URLParser urlParser = URLParser();
-		WebSocket socket = WebSocket();
-		HtmlParser htmlParser = HtmlParser();
-
-		url = new char[strlen(argv[1])];
-		strcpy(url, argv[1]);
-		printf("URL: %s\n", url);
-
-		// parse url string into an HTTP GET request
-		request = urlParser.parseURLString(url);
-		hostname = (char*)urlParser.getHostname(url);
-
-		// create a web client to send our request
-		socket.Setup(hostname, 80);
-		socket.Send(request);
-
-		// and to get the resulting HTML file
-		htmlFileName = "test.html";
-		statusCode = socket.ReadAndWriteToFile(htmlFileName);
-
-		// TODO: just pass whole buffer instead of saving as a file first
-
-		if (200 < statusCode && statusCode < 300)
-		{
-			// parse the HTML file to count number of links
-			htmlParser.parse(htmlFileName, url);
-		}
-
-		printf("\n----------------------------\n");
-		printf("HTTP/1.0 %d", statusCode);
-
-		printf("\n\n\n");
-		
-
-	}
-	else if (argc == 3)
-	{
-		// part 2 functionality
-
 		// > hw1.exe <NUM-THREADS> <URL-INPUT.TXT>
 
-		// First param
+		// First param - thread count
 		istringstream in(argv[1]);
 		int numThreads;
 		if (in >> numThreads && in.eof())
@@ -76,26 +27,20 @@ int _tmain(int argc, _TCHAR* argv[])
 			cout << "Error parsing number of threads" << endl;
 		}
 
-		// Second param
+		// Second param - file name
 		string inputFile = argv[2];
 		cout << "Provided input file: " << inputFile << endl;
 
-
-		if (numThreads != 1)
-		{
-			cout << "Invalid input for number of threads to spawn" << endl;
-			cout << "\tExpected: 1, but found " << numThreads << endl;
-			cout << "Please provide input of the form:\n\t> hw1.exe <NUM-THREADS> <URL-INPUT.TXT>" << endl;
-			return 0;
-		}
-
 		FileParser fileParser = FileParser();
 		fileParser.parse(inputFile, numThreads);
-
+	}
+	else
+	{
+		cout << "Invalid number of arguments provided" << endl;
+		cout << "Usage:\n\t> hw1.exe <NUM-THREADS> <URL-INPUT.TXT>" << endl;
 	}
 
-	printf("\n\nPress enter key to continue");
-	scanf("\n");
+	cin.get();
 	
 	return 0;
 }
